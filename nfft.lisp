4080c44d858189bec0a1b6951eab548a5f8a90e7
Martin Kielhorn <martin.kielhorn@ezono.com>
2016-06-01 02:02:32 +0200

    puh. the headers really need manual testing with c2ffi to get rid of all errors. also the spec files must be deleted before each run

--- nfft.lisp
+++ nfft.lisp
@@ -8,19 +8,40 @@
 					     (user-homedir-pathname))))
 
 ;; https://www-user.tu-chemnitz.de/~potts/nfft/guide3/html/node38.html
+(progn
+  (with-open-file (s "/tmp/nfft0.h"
+                     :direction :output
+                     :if-does-not-exist :create
+                     :if-exists :supersede)
+    (format s "#include \"/usr/lib/gcc/x86_64-pc-linux-gnu/6.1.1/include/stddef.h\"~%") ;; ptrdiff_t
+    ;;(format s "#define NFFT_INT int")
+    (format s "#include \"/usr/local/include/nfft3.h\"~%"))
+  (autowrap::run-check autowrap::*c2ffi-program*
+                       (autowrap::list "/tmp/nfft0.h"
+                                       "-D" "null"
+                                       "-M" "/tmp/nfft_macros.h"
+                                       "-A" "x86_64-pc-linux-gnu"))
+  
+  (with-open-file (s "/tmp/nfft1.h"
+                     :direction :output
+                     :if-does-not-exist :create
+                     :if-exists :supersede)
+    
+    (format s "#include \"/tmp/nfft0.h\"~%")
+    (format s "#include \"/tmp/nfft_macros.h\"~%")))
 
-(autowrap:c-include "/usr/local/include/nfft3.h"
-		    :spec-path *spec-path*
-		    :exclude-arch ("arm-pc-linux-gnu"
-				   "i386-unknown-freebsd"
-				   "i686-apple-darwin9"
-				   "i686-pc-linux-gnu"
-				   "i686-pc-windows-msvc"
-				   "x86_64-apple-darwin9"
-					;"x86_64-pc-linux-gnu"
-				   "x86_64-pc-windows-msvc"
-				   "x86_64-unknown-freebsd")
-		    :exclude-sources ( "/usr/include/_G_config.h"
+(autowrap:c-include "/tmp/nfft1.h"
+                    :spec-path *spec-path*
+                    :exclude-arch ("arm-pc-linux-gnu"
+                                   "i386-unknown-freebsd"
+                                   "i686-apple-darwin9"
+                                   "i686-pc-linux-gnu"
+                                   "i686-pc-windows-msvc"
+                                   "x86_64-apple-darwin9"
+                                        ;"x86_64-pc-linux-gnu"
+                                   "x86_64-pc-windows-msvc"
+                                   "x86_64-unknown-freebsd")
+                    :exclude-sources ( "/usr/include/_G_config.h"
  "/usr/include/bits/stdio_lim.h"
  "/usr/include/bits/sys_errlist.h"
  "/usr/include/bits/types.h"
@@ -34,7 +55,10 @@
  "/usr/include/stdio.h"
  "/usr/include/sys/cdefs.h"
  "/usr/include/wchar.h")
-		    :trace-c2ffi t)
+		    :include-sources ("/usr/include/stddev.h")
+                    :trace-c2ffi t)
+
+
 
 (cffi:load-foreign-library "/usr/local/lib/libnfft3.so")
 
@@ -49,7 +73,7 @@
      (progn
        (nfft-init-1d plan N M)
        (nfft-vrand-shifed-unit-double (nfft-plan.x plan)
-				      (nfft-plan.m-total plan))
+				      (nfft-plan. plan))
        (setf (nfft-plan.flags plan) +PRE-ONE-PSI+)
        (let* ((xl (loop for i below M ;; *d  , values in -.5 .. .5
 		     collect
